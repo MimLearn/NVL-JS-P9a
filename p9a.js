5e7b3c19b204a045cbbab7f509d569c53d8a9854
MimLearn <51676988+MimLearn@users.noreply.github.com>
2019-06-25 12:09:51 +0200

    p9a.js

---
+++ p9a.js
@@ -0,0 +1,7 @@
+<script>
+var vuelos = [['Madrid-Roma',80],['Madrid-Berlin',60],['Madrid-Croacia',120],['Madrid-Budapest',45],['Madrid-Londres',63]];
+var longitudArray = vuelos.length;
+var longitudPrimerElemento = vuelos[0].length; // 2
+alert(vuelos[2]); // Madrid-Croacia 120
+alert(vuelos[2][1]); // 120
+</script>
